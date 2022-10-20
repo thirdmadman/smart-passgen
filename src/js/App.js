7fc66d200f838ce70712046cf98864489d495312
thirdmadman <thirdmadman@gmail.com>
2022-10-21 02:49:14 +0600

    feat: impl basic interface

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,91 @@
 import dhc from './utils/dch';
 import SmartPassGen from './services/SmartPassGen';
+import ToggleElement from './components/ToggleElement';
+import InputNumber from './components/InputNumber';
+import LocalStorageProvider from './services/LocalStorageProvider';
 
 export default class App {
   constructor() {
+    const configData = LocalStorageProvider.getData();
+
+    const appTitle = dhc('div', ['app-title'], 'Smart PassGen');
+
+    const configGroup = dhc('div', ['config-group']);
+
+    const smartPassGen = new SmartPassGen(configData.config.generatorConfig);
+
     this.mainEl = document.querySelector('.app');
-    const buttonGenerate = dhc('button', ['button-generate'], 'Generate');
-    const smartPassGen = new SmartPassGen();
-    buttonGenerate.onclick = () => this.mainEl.append(dhc('div', ['pass'], smartPassGen.generate()));
-    this.mainEl.append(buttonGenerate);
+    const buttonGeneratePassword = dhc('button', ['button-generate'], 'Generate');
+    const currentPasswordText = dhc('div', ['password-text'], 'Here will be your password');
+
+    const toggleOption = (optionName, event) => {
+      smartPassGen.options[optionName] = event.target.checked;
+      configData.config.generatorConfig[optionName] = event.target.checked;
+      LocalStorageProvider.setData(configData);
+    };
+
+    const isSmartSymbolsToggle = new ToggleElement({
+      textContent: 'Smart generator',
+      onclick: (e) => toggleOption('isSmartSymbols', e),
+      currentState: smartPassGen.options.isSmartSymbols,
+    });
+
+    const isUseLettersToggle = new ToggleElement({
+      textContent: 'Include letters',
+      onclick: (e) => toggleOption('isUseLetters', e),
+      currentState: smartPassGen.options.isUseLetters,
+    });
+
+    const isIncludeUppercaseToggle = new ToggleElement({
+      textContent: 'Include uppercase letters',
+      onclick: (e) => toggleOption('isIncludeUppercase', e),
+      currentState: smartPassGen.options.isIncludeUppercase,
+    });
+
+    const isUseNumbersToggle = new ToggleElement({
+      textContent: 'Include numbers',
+      onclick: (e) => toggleOption('isUseNumbers', e),
+      currentState: smartPassGen.options.isUseNumbers,
+    });
+
+    const isUseSpecialSymbols = new ToggleElement({
+      textContent: 'Include special symbols',
+      onclick: (e) => toggleOption('isUseSpecialSymbols', e),
+      currentState: smartPassGen.options.isUseSpecialSymbols,
+    });
+
+    const isUseSpecialSymbolsAdvancedToggle = new ToggleElement({
+      textContent: 'Include special symbols (advanced)',
+      onclick: (e) => toggleOption('isUseSpecialSymbolsAdvanced', e),
+      currentState: smartPassGen.options.isUseSpecialSymbolsAdvanced,
+    });
+
+    buttonGeneratePassword.onclick = () => {
+      currentPasswordText.innerText = smartPassGen.generate();
+    };
+
+    const inputNumber = new InputNumber({
+      textContent: 'Input Number',
+      onchange: (e) => {
+        smartPassGen.options.length = e.target.value;
+        configData.config.generatorConfig.length = Number.parseInt(e.target.value, 10);
+        LocalStorageProvider.setData(configData);
+      },
+      value: smartPassGen.options.length,
+    });
+
+    this.mainEl.append(appTitle);
+    this.mainEl.append(buttonGeneratePassword);
+    this.mainEl.append(currentPasswordText);
+
+    configGroup.append(inputNumber.getElement());
+    configGroup.append(isSmartSymbolsToggle.getElement());
+    configGroup.append(isUseLettersToggle.getElement());
+    configGroup.append(isIncludeUppercaseToggle.getElement());
+    configGroup.append(isUseNumbersToggle.getElement());
+    configGroup.append(isUseSpecialSymbols.getElement());
+    configGroup.append(isUseSpecialSymbolsAdvancedToggle.getElement());
+
+    this.mainEl.append(configGroup);
   }
 }
